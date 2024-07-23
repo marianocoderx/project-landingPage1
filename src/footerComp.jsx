af84b47471c205550b8157ed5119b180ab042fac
marianobugallo100 <marianobugallo100@gmail.com>
2024-07-23 18:45:27 +0000

    footer editing

--- src/footerComp.jsx
+++ src/footerComp.jsx
@@ -1,45 +1,49 @@
 import React from "react";
 import '../stylesheets/footercomp.css';
 
-export default function FooterComp (){
-    return (
-        <>
-        <footer className="footerContainer">
+export default function FooterComp() {
+  return (
+    <>
+      <footer className="footerContainer">
+        <div>
+          Nombre de la compañía. Todos los derechos reservados.
+        </div>
 
-            <div>
-                
-            </div>
+        <div>
+          <ul>
+            <li>Historia</li>
+            <li>Misión-visión</li>
+            <li>Equipo</li>
+            <li>Valores</li>
+            <li>Logros</li>
+          </ul>
+        </div>
 
-            <div>
-                <ul>
-                    <li>Historia</li>
-                    <li>Mision-vision</li>
-                    <li>Equipo</li>
-                    <li>Valores</li>
-                    <li>Logros</li>
-                </ul>
-            </div>
+        <div>
+          <ul>
+            <li>Introducción</li>
+            <li>Últimas noticias</li>
+            <li>Destacados</li>
+            <li>Testimonios</li>
+          </ul>
+        </div>
 
-            <div>
-                <ul>
-                    <li>Introduccion</li>
-                    <li>Ultimas noticias</li>
-                    <li>Destacados</li>
-                    <li>Testimonios</li>
-                </ul>
-            </div>
+        <div>
+          <ul>
+            <li>Consultoría</li>
+            <li>Desarrollo de software</li>
+            <li>Soporte técnico</li>
+            <li>Capacitación</li>
+            <li>IT Solutions</li>
+          </ul>
+        </div>
 
-            <div>
-            <ul>
-                    <li>Consultoria</li>
-                    <li>Desarrollo de software</li>
-                    <li>Soporte tecnico</li>
-                    <li>Capacitacion</li>
-                    <li>IT Solutions</li>
-                </ul>
-            </div>
-
-        </footer>
-        </>
-    )
+        <div className="companyDetails">
+          <p>© 2024 Nombre de la Compañía. Todos los derechos reservados.</p>
+          <p>Dirección: Calle Falsa 123, Ciudad, País</p>
+          <p>Contacto: contacto@compania.com | +1 234 567 890</p>
+        </div>
+      </footer>
+    </>
+  );
 }
